e005ba08e79d06942c1c8e14647e3ce3fb2e06a0
bali.appkey <bali.appkey@gmail.com>
2021-06-25 15:50:29 +0800

    Update webhook

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('webhook', function () {
+    return true;
+});
+
+// Route::get('/setwebhook', function () {
+// 	$response = Telegram::setWebhook(['url' => 'https://d79e77f5.ngrok.io/42yUojv1YQPOssPEpn5i3q6vjdhh7hl7djVWDIAVhFDRMAwZ1tj0Og2v4PWyj4PZ/webhook']);
+// 	dd($response);
+// });
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('curl', function () {
+    $url = "https://girliemac.com/assets/images/avatar.jpg";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    // don't download content
+    curl_setopt($ch, CURLOPT_NOBODY, 1);
+    curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    dd($result);
+});
